cb9d4ab21fe9d233b3cc1026a07378333a54e61c
Kristian <kristian.nasr@student.tabyenskilda.se>
2024-02-11 19:27:01 +0100

    instagram integration

---
+++ js/instagram.js
@@ -0,0 +1,6 @@
+/* curator-feed-default-feed-layout */
+(function(){
+    var i,e,d=document,s="script";i=d.createElement("script");i.async=1;i.charset="UTF-8";
+    i.src="https://cdn.curator.io/published/88f2276e-6a62-4e17-acba-072522bdea70.js";
+    e=d.getElementsByTagName(s)[0];e.parentNode.insertBefore(i, e);
+    })();
